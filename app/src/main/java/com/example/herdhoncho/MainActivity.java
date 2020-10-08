8eaf6a5bdfe455d8ce0a07e5666e8c9483343819
Carmen <carmens@dotcomsoftware.co.za>
2020-10-08 09:19:04 +0200

    Install OpenCV

---
+++ app/src/main/java/com/example/herdhoncho/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.herdhoncho;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import org.opencv.dnn.Net;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
